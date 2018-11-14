265db09c5a7bc8d2b0aaf53ee95a2c573979e36e
Nyxgen <nyxgen471@gmail.com>
2018-11-14 20:35:24 +0100

    Dodanie kilku konwersji, poprawa czytelności kodu.

--- JavaTeX/src/Main.java
+++ JavaTeX/src/Main.java
@@ -4,39 +4,79 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Running ver 1 (wiki-script -> .tex) . . .");
         String path = "C:/Users/Nyxgen/Downloads/JavaTeX/JavaTeX/src/Data/";
-        String fileOutput = "output1.tex";
-        String fileScript = "MediaWikiExample.txt";
-        String command = "pandoc " + path + fileScript + " " + "-f mediawiki -t latex --standalone -o " + path + fileOutput;
+        String txtToTex = "pandoc " + path + "MediaWikiExample.txt" + " " + "-f mediawiki -t latex --standalone -o " + path + "MediaWikiExample.tex";
+        String txtToPdf = "pandoc -s " + path + "file.txt -o " + path + "txt.pdf";
+        String texToPdf = "pandoc " + path + "output1.tex" + " " + "--pdf-engine=xelatex -o " + path + "output1.pdf";
+        String texToText = "pandoc -s " + path + "output1.tex -o " + path + "output1.text";
+        String texToTxt = "pandoc -s " + path + "output1.tex -o " + path + "output1.txt";
+        String textToHtml = "pandoc " + path + "output1.tex -s -o " + path + "output1.html";
 
-        int process = Runtime.getRuntime().exec(command).waitFor();
-        if(process == 0)
+
+
+        System.out.println("Converting .txt -> .tex");
+        Process pr1 = Runtime.getRuntime().exec(txtToTex);
+        processIndicator(pr1);
+        processEndCode(pr1.waitFor());
+
+        System.out.println("Converting .tex -> .pdf");
+        Process pr2  = Runtime.getRuntime().exec(texToPdf);
+        processIndicator(pr2);
+        processEndCode(pr2.waitFor());
+
+        System.out.println("Converting .txt -> .pdf ");
+        Process pr3 = Runtime.getRuntime().exec(txtToPdf);
+        processIndicator(pr3);
+        processEndCode(pr3.waitFor());
+
+        System.out.println("Converting .tex -> .text ");
+        Process pr4 = Runtime.getRuntime().exec(texToText);
+        processIndicator(pr4);
+        processEndCode(pr4.waitFor());
+
+        System.out.println("Converting .text -> .html ");
+        Process pr5 = Runtime.getRuntime().exec(textToHtml);
+        processIndicator(pr5);
+        processEndCode(pr5.waitFor());
+
+        System.out.println("Converting .text -> .html ");
+        Process pr6 = Runtime.getRuntime().exec(texToTxt);
+        processIndicator(pr6);
+        processEndCode(pr6.waitFor());
+    }
+
+    public static void processIndicator(Process p)
+    {
+        for(int i = 0; p.isAlive(); ++i)
+        {
+            if(i/10==0)
+            {
+                System.out.print("\\ \r");
+            }
+            if(i/10==1)
+            {
+                System.out.print("| \r");
+            }
+            if(i/10==2)
+            {
+                System.out.print("/ \r");
+            }
+            if(i/10==3)
+            {
+                System.out.print("- \r");
+            }
+            if(i==40) i = 0;
+
+        }
+    }
+
+    public static void processEndCode(int p)
+    {
+        if(p == 0)
         {
             System.out.println("Conversion executed successfully!");
         }
-        else System.out.println("Conversion failed! Error code: " + process);
-
-
-        System.out.println("Converting .tex -> .pdf . . .");
-       // command = "pdflatex -output-directory " + path + " " + path + "output1.tex";
-        command = "pandoc " + path + "output1.tex" + " " + "--pdf-engine=xelatex -o " + path + "output1.pdf";
-        process = Runtime.getRuntime().exec(command).waitFor();
-        if(process == 0)
-        {
-            System.out.println("Conversion executed successfully!");
-        }
-        else System.out.println("Conversion failed! Error code: " + process);
-
-        System.out.println("Converting .txt -> .pdf . . .");
-        command = "pandoc -s " + path + "file.txt -o " + path + "txt.pdf";
-
-        process = Runtime.getRuntime().exec(command).waitFor();
-        if(process == 0)
-        {
-            System.out.println("Conversion executed successfully!");
-        }
-        else System.out.println("Conversion failed! Error code: " + process);
+        else System.out.println("Conversion failed! Error code: " + p);
     }
 }
 

775e2e0c923ea94157f0459ef625a1668246dba5
Bartłomiej Smuga <smuga@nonstop.co>
2018-11-15 15:31:33 +0100

    output file name fix

--- src/main/java/io/controllers/ConvertController.java
+++ src/main/java/io/controllers/ConvertController.java
@@ -17,7 +17,7 @@ public class ConvertController {
     @RequestMapping(value = "/txt-tex", method = RequestMethod.GET)
     public String convertTxtToTex (@RequestParam("inputFileName") String inputFileName) throws IOException, InterruptedException {
         Converter converter = new Converter(IoApplication.sourcePath);
-        String outputFileName = inputFileName + "out";
+        String outputFileName = inputFileName.split("\\.")[0] + "out.tex";
         int conversionResult = converter.run(ConvertTypes.TXT_TO_TEX, inputFileName, outputFileName);
         return conversionResult == 0 ? "TXT => TEX Success" : "Error occured! Conversion failed";
     }
@@ -25,7 +25,7 @@ public class ConvertController {
     @RequestMapping(value = "/txt-pdf", method = RequestMethod.GET)
     public String convertTxtToPdf (@RequestParam("inputFileName") String inputFileName) throws IOException, InterruptedException {
         Converter converter = new Converter(IoApplication.sourcePath);
-        String outputFileName = inputFileName + "out";
+        String outputFileName = inputFileName.split("\\.")[0] + "out.pdf";
         int conversionResult = converter.run(ConvertTypes.TXT_TO_PDF, inputFileName, outputFileName);
         return conversionResult == 0 ? "TXT => PDF Success" : "Error occured! Conversion failed";
     }
@@ -33,7 +33,7 @@ public class ConvertController {
     @RequestMapping(value = "/tex-pdf", method = RequestMethod.GET)
     public String convertTexToPdf (@RequestParam("inputFileName") String inputFileName) throws IOException, InterruptedException {
         Converter converter = new Converter(IoApplication.sourcePath);
-        String outputFileName = inputFileName + "out";
+        String outputFileName = inputFileName.split("\\.")[0] + "out.pdf";
         int conversionResult = converter.run(ConvertTypes.TEX_TO_PDF, inputFileName, outputFileName);
         return conversionResult == 0 ? "TEX => PDF Success" : "Error occured! Conversion failed";
     }
@@ -41,7 +41,7 @@ public class ConvertController {
     @RequestMapping(value = "/tex-txt", method = RequestMethod.GET)
     public String convertTexToTxt (@RequestParam("inputFileName") String inputFileName) throws IOException, InterruptedException {
         Converter converter = new Converter(IoApplication.sourcePath);
-        String outputFileName = inputFileName + "out";
+        String outputFileName = inputFileName.split("\\.")[0] + "out.txt";
         int conversionResult = converter.run(ConvertTypes.TEX_TO_TXT, inputFileName, outputFileName);
         return conversionResult == 0 ? "TEX => TXT Success" : "Error occured! Conversion failed";
     }
--- src/main/java/io/dao/ExampleDAO.java
+++ src/main/java/io/dao/ExampleDAO.java
@@ -1,4 +1,4 @@
-package io.daos;
+package io.dao;
 
 public class ExampleDAO {
 }
